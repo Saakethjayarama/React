17f96bd9dfde717680b3254dda3f2e22e8979327
saaketh <saakethaj@gmail.com>
2020-11-30 15:06:45 +0530

    dets php added, react project created

--- mini projects/employees/src/AddDepartmentModal.js
+++ mini projects/employees/src/AddDepartmentModal.js
@@ -1,38 +1,52 @@
-import React, {useState} from 'react'
-import { Modal, Row, Col, Form, Button } from 'react-bootstrap'
-import TextField from '@material-ui/core/TextField';
-
+import React, { useState } from "react";
+import { Modal, Row, Col, Form, Button } from "react-bootstrap";
+import TextField from "@material-ui/core/TextField";
 
 function AddDepartmentModal(props) {
-    const {show, onHide, onSubmit} = props;
+  const { show, onHide, onSubmit } = props;
 
-    const [deptName, setDeptName] = useState('');
+  const [deptName, setDeptName] = useState("");
 
-    
-    return (
-        <Modal
-            show={show}
-            onHide={onHide}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add Department
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="container">
+          <TextField
+            id="outlined-basic"
+            label="Department Name"
+            variant="outlined"
+            value={deptName}
+            onChange={(event) => setDeptName(event.target.value)}
+          />
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          onClick={props.onHide}
+          variant="warning"
+          onClick={() => {
+            onSubmit(deptName);
+          }}
         >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Add Department
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div className="container">
-                    <TextField id="outlined-basic" label="Department Name" variant="outlined" value={deptName} onChange={(event) => setDeptName(event.target.value)}/>
-                </div>              
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide} variant="warning" onClick={() => {onSubmit(deptName)}}>Add</Button>
-                <Button onClick={props.onHide} variant="danger">Close</Button>
-            </Modal.Footer>
-        </Modal>
-    )
+          Add
+        </Button>
+        <Button onClick={props.onHide} variant="danger">
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
-export default AddDepartmentModal
+export default AddDepartmentModal;
